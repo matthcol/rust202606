3b46cde78e7431e69a3c2b0dfb6e1b258b192d7d
matthcol <matthias.colin@gmail.com>
2026-06-25 11:55:05 +0200

    par lines

--- 05-meteo/src/main.rs
+++ 05-meteo/src/main.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::process;
 use std::io::{BufRead, BufReader, Result};
 
 // stack with a lot of match Ok/Err
-fn handle_meteo_file(f: File) -> std::result::Result<(String, Vec<String>), String>{
+fn _handle_meteo_file(f: File) -> std::result::Result<(String, Vec<String>), String>{
     let mut buf = BufReader::new(f);
     let mut headers = String::new();
     if let Ok(_size) = buf.read_line(&mut headers){
@@ -17,11 +18,11 @@ fn handle_meteo_file(f: File) -> std::result::Result<(String, Vec<String>), Stri
     }
 } // file is closed
 
-fn read_meteo_file(filename: &str){
+fn _read_meteo_file(filename: &str){
     let file_result = File::open(filename);
     match file_result {
         Ok(f) => {
-            match handle_meteo_file(f) {
+            match _handle_meteo_file(f) {
                 Ok((headers, lines)) => {
                     println!("File read with sucess");
                     println!("Headers: {headers}");
@@ -40,6 +41,32 @@ fn read_meteo_file(filename: &str){
 }
 
 // Stack avec propagation d'erreur au niveau de chaque fonction
+fn parse_lines(headers: String, lines: Vec<String>, columns: &[&str]) -> Option<()>{
+    let mut column_indexes: HashMap<&str, usize> = HashMap::new();
+    let header_vec: Vec<&str> = headers.split(";").collect();
+    for column in columns{
+        if let Some(index) = header_vec.iter().position(|header| header == column){
+            column_indexes.insert(column, index);
+        } else {
+            // TODO: gestion d'erreur
+            eprintln!("[warning] {column} not found in headers");
+        }
+    }
+    println!("{column_indexes:?}");
+    let data: Vec<HashMap<&str, &str>> = lines.iter()
+        .map(|line| {
+            let line_vec: Vec<&str> = line.split(";").collect();
+            let mesure: HashMap<&str, &str> = column_indexes.iter()
+                .map(|(col, index)| Some((*col, *line_vec.get(*index)?)))
+                .collect::<Option<HashMap<_, _>>>()?;
+            Some(mesure)
+        })
+        .collect::<Option<Vec<_>>>()?;
+
+    println!("First data parsed: {:?}", data.get(0));
+    Some(())
+}
+
 fn handle_meteo_file_simple(f: File) -> Result<(String, Vec<String>)>{
     let mut buf = BufReader::new(f);
     let mut headers = String::new();
@@ -57,11 +84,12 @@ fn read_meteo_file_simple(filename: &str) -> Result<()>{
     println!("Headers: {headers}");
     println!("First line: {:?}", lines.get(0));
     println!("Data count: {}", lines.len());
+    parse_lines(headers, lines, &["NUM_POSTE", "NOM_USUEL", "AAAAMMJJHH", "T"]);
     Ok(())
 }
 
 fn main() {
-    read_meteo_file("data/H_31_latest-2025-2026.csv");
+    // _read_meteo_file("data/H_31_latest-2025-2026.csv");
     if let Err(error) = read_meteo_file_simple("data/H_31_latest-2025-2026.csv") {
         eprintln!("Error while reading csv: {error}");
     }
